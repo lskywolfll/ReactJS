68308c58a08f288cfb74539aae0f5c427dd48856
lskywolfll <renealejandrosanchezmorales@gmail.com>
2020-03-22 01:10:49 -0300

    se hace destructurarion de los props en el badge (component) y agregacion de enmmet para usar el intellisense en jsx

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -10,6 +10,17 @@ class Badge extends React.Component {
         // Son propiedades como los que tiene html,css con su class, src, type y etc, por ende podemos crear propiedades unicas(se puede ver como etiqueta) y nosotros le damos un valor mediante el llamado a este componente ej: <component propiertie="value" /> o <Badge nombre="rene" />
         // this.props;
 
+        // Tambien podemos hacer destructuracion de las propiedads que nos llegan y las convertimos en variables
+        // NT: Deben ser declaradas antes del return
+        const
+            {
+                firstName,
+                lastName,
+                jobTitle,
+                twitter,
+                avatarUrl
+            } = this.props;
+
         // Por convenciones para agregar los estilos deseados para el html que tengamos en nuestros componentes debemos usar classname en vez de class
         // Esto es por que ya js tiene una palabra clave llamada class por eso nos recomienda que mejor usemos otro que es el classname
         return <div className="Badge">
@@ -19,13 +30,13 @@ class Badge extends React.Component {
             </div>
 
             <div className="Badge__section-name">
-                <img className="rounded-circle Badge_avatar" src={this.props.avatarUrl} alt="Avatar" />
-                <h1>{this.props.firstName} <br /> {this.props.lastName}</h1>
+                <img className="rounded-circle Badge_avatar" src={avatarUrl} alt="Avatar" />
+                <h1>{firstName} <br /> {lastName}</h1>
             </div>
 
             <div className="Badge__section-info">
-                <h3>{this.props.jobTitle}</h3>
-                <div>@{this.props.twitter}</div>
+                <h3>{jobTitle}</h3>
+                <div>@{twitter}</div>
             </div>
 
             <div className="Badge__footer">#PlatziConf</div>
