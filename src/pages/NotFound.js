1c3797009432b1c21c43248db2458687f79d072b
lskywolfll <renealejandrosanchezmorales@gmail.com>
2020-03-25 20:34:28 -0300

    se termina la pagina not found

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './styles/NotFound.css';
+import NotFoundImage from '../images/img404.png';
 
 function NotFound() {
     return (
         <div className="NotFound">
-            <h1 className="NotFound__title">404: Not Found </h1>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <img
+                            src={NotFoundImage}
+                            className="NotFound__image"
+                            alt="Hombre en el espacio donde flota y con una caja en la parte de abajo" />
+                        <h1 className="NotFound__title">404: Not Found</h1>
+                    </div>
+                    <div className="col-10">
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
