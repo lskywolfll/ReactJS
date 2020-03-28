205b293a2e011b8bfa5a656d3bfd22b15ed83b80
lskywolfll <renealejandrosanchezmorales@gmail.com>
2020-03-27 22:40:37 -0300

    se completo un ciclo de vida de un componente que es Badges :O

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -5,40 +5,74 @@ import BadgesList from '../components/BadgesList';
 import { Link } from 'react-router-dom';
 
 class Badges extends React.Component {
+    // Se crea al artista y el escenario
+    constructor(props) {
+        // Se heredan todos los props y cuando lleguen nuevos tambien se tendran por lo cual podremos usarlos cuando se desmonten nuestros componentes
+        super(props);
+        this.state = {
+            data: []
+        }
+        console.log('1. constructor()');
+    }
+    // El artista se pone un traje y se abre el telon
+    componentDidMount(){
+        console.log('3. componentDidMount()');
+        // De esta manera tambien podemos crear un id de timeOut
+       this.timeoutId = setTimeout(() => {
+            this.setState({
+                data: [
+                    {
+                      "id": "2de30c42-9deb-40fc-a41f-05e62b5939a7",
+                      "firstName": "Freda",
+                      "lastName": "Grady",
+                      "email": "Leann_Berge@gmail.com",
+                      "jobTitle": "Legacy Brand Director",
+                      "twitter": "FredaGrady22221-7573",
+                      "avatarUrl": "https://www.gravatar.com/avatar/f63a9c45aca0e7e7de0782a6b1dff40b?d=identicon"
+                    },
+                    {
+                      "id": "d00d3614-101a-44ca-b6c2-0be075aeed3d",
+                      "firstName": "Major",
+                      "lastName": "Rodriguez",
+                      "email": "Ilene66@hotmail.com",
+                      "jobTitle": "Human Research Architect",
+                      "twitter": "MajorRodriguez61545",
+                      "avatarUrl": "https://www.gravatar.com/avatar/d57a8be8cb9219609905da25d5f3e50a?d=identicon"
+                    },
+                    {
+                      "id": "63c03386-33a2-4512-9ac1-354ad7bec5e9",
+                      "firstName": "Daphney",
+                      "lastName": "Torphy",
+                      "email": "Ron61@hotmail.com",
+                      "jobTitle": "National Markets Officer",
+                      "twitter": "DaphneyTorphy96105",
+                      "avatarUrl": "https://www.gravatar.com/avatar/e74e87d40e55b9ff9791c78892e55cb7?d=identicon"
+                    },
+                  ]
+            })
+        },3000)
+    }
+    // El artista cambia de traje y color
+    componentDidUpdate(prevProps, prevState){
+        console.log('5. componentDidUpdate');
+        console.log({
+            prevProps,
+            prevState
+        });
 
-    state = {
-        data: [
-            {
-              "id": "2de30c42-9deb-40fc-a41f-05e62b5939a7",
-              "firstName": "Freda",
-              "lastName": "Grady",
-              "email": "Leann_Berge@gmail.com",
-              "jobTitle": "Legacy Brand Director",
-              "twitter": "FredaGrady22221-7573",
-              "avatarUrl": "https://www.gravatar.com/avatar/f63a9c45aca0e7e7de0782a6b1dff40b?d=identicon"
-            },
-            {
-              "id": "d00d3614-101a-44ca-b6c2-0be075aeed3d",
-              "firstName": "Major",
-              "lastName": "Rodriguez",
-              "email": "Ilene66@hotmail.com",
-              "jobTitle": "Human Research Architect",
-              "twitter": "MajorRodriguez61545",
-              "avatarUrl": "https://www.gravatar.com/avatar/d57a8be8cb9219609905da25d5f3e50a?d=identicon"
-            },
-            {
-              "id": "63c03386-33a2-4512-9ac1-354ad7bec5e9",
-              "firstName": "Daphney",
-              "lastName": "Torphy",
-              "email": "Ron61@hotmail.com",
-              "jobTitle": "National Markets Officer",
-              "twitter": "DaphneyTorphy96105",
-              "avatarUrl": "https://www.gravatar.com/avatar/e74e87d40e55b9ff9791c78892e55cb7?d=identicon"
-            },
-          ]
+        console.log({
+            props: this.props,
+            state: this.state
+        });
+    }
+    // Sale de escena el artista y se hace limpieza
+    componentWillUnmount(){
+        console.log('6. componentWillUnmount');
+        clearTimeout(this.timeoutId);
     }
 
     render() {
+        console.log('2/4. render()');
         return (
             <React.Fragment>
                 <div className="Badges">
