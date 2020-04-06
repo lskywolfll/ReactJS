311bc7b978468c474414433488f1537d4bd31a1a
lskywolfll <renealejandrosanchezmorales@gmail.com>
2020-04-05 23:14:57 -0400

    se crea un portal que es practicamente similar al render de react, pero enfocado a fuera osea en el html donde utilizamos nuestra spa podemos agregar un div con un id modal para poder evitarnos todos los problemas que puedan ocurrir por estar siempre en el mismo dom para los estilos por lo cual eficientemente podemos evitarnos esos problemas para poder estar tranquilos que todo se vera muy bien con el design que queramos

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import Layout from '../components/Layout';
 import BadgeNew from '../pages/BadgeNew';
 import Badges from '../pages/Badges';
 import BadgeEdit from '../pages/BadgeEdit';
-import BadgeDetails from '../pages/BadgeDetails';
+import BadgeDetailsContainer from '../pages/BadgeDetailsContainer';
 import NotFound from '../pages/NotFound';
 import Home from '../pages/Home';
 
@@ -22,7 +22,7 @@ function App() {
                 */}
                     <Route exact path="/badges" component={Badges} />
                     <Route exact path="/badges/new" component={BadgeNew} />
-                    <Route exact path="/badges/:badgeId" component={BadgeDetails} />
+                    <Route exact path="/badges/:badgeId" component={BadgeDetailsContainer} />
                     <Route exact path="/badges/:badgeId/edit" component={BadgeEdit} />
                     <Route exact path="/" component={Home} />
                     <Route component={NotFound}/>
--- src/pages/BadgeDetails.js
+++ src/pages/BadgeDetails.js
@@ -1,116 +1,74 @@
-import React, { Component } from 'react';
+import React from 'react'
+import ReactDOM from 'react-dom';
 import './styles/BadgeDetails.css';
-import confLogo from '../images/platziconf-logo.svg'
-import PageLoading from '../components/PageLoading';
-import PageError from '../components/PageError';
-import Badge from '../components/Badge';
-import api from '../api';
 import { Link } from 'react-router-dom';
+import Badge from '../components/Badge';
+import confLogo from '../images/platziconf-logo.svg'
 
-export class BadgeDetails extends Component {
+function BadgeDetails(props) {
+    
+    const {
+        firstName,
+        lastName,
+        jobTitle,
+        twitter,
+        avatarUrl,
+        email,
+        id
+    } = props.badge
 
-    state = {
-        loading: true,
-        error: null,
-        data: undefined
-    }
-
-    componentDidMount() {
-        this.obtenerBadge()
-    }
-
-    obtenerBadge = async () => {
-
-        this.setState({
-            loading: true,
-            error: null
-        });
-
-        try {
-            const data = await api.badges.read(this.props.match.params.badgeId);
-            this.setState({
-                loading: false,
-                data: data
-            });
-        } catch (error) {
-            this.setState({
-                loading: false,
-                error: error
-            });
-        }
-    }
-
-    render() {
-
-        if (this.state.loading) {
-            return <PageLoading />
-        }
-
-        if (this.state.error) {
-            return <PageError error={this.state.error} />
-        }
-
-        const {
-            firstName,
-            lastName,
-            jobTitle,
-            twitter,
-            avatarUrl,
-            email,
-            id
-        } = this.state.data
-
-        return (
-            <div>
-                <div className="BadgeDetails__hero">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-6">
-                                <img src={confLogo} alt="Logo de la conferencia" />
-                            </div>
-                            <div className="col-6 BadgeDetails__hero-attendant-name">
-                                <h1>{firstName} {lastName}</h1>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
+    return (
+        <div>
+            <div className="BadgeDetails__hero">
                 <div className="container">
                     <div className="row">
-                        <div className="col">
-                            <Badge
-                                firstName={firstName}
-                                lastName={lastName}
-                                jobTitle={jobTitle}
-                                twitter={twitter}
-                                avatarUrl={avatarUrl}
-                                email={email}
-                            />
+                        <div className="col-6">
+                            <img src={confLogo} alt="Logo de la conferencia" />
                         </div>
-                        <div className="col">
-                            <h2>Actions</h2>
-                            <div>
-                                <Link
-                                    className="btn btn-primary mb-4"
-                                    to={`/badges/${id}/edit`}
-                                >
-                                    Edit
-                            </Link>
-                            </div>
-
-                            <div>
-                                <button
-                                    className="btn btn-danger"
-                                >
-                                    Delete
-                                </button>
-                            </div>
+                        <div className="col-6 BadgeDetails__hero-attendant-name">
+                            <h1>{firstName} {lastName}</h1>
                         </div>
                     </div>
                 </div>
             </div>
-        )
-    }
+
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <Badge
+                            firstName={firstName}
+                            lastName={lastName}
+                            jobTitle={jobTitle}
+                            twitter={twitter}
+                            avatarUrl={avatarUrl}
+                            email={email}
+                        />
+                    </div>
+                    <div className="col">
+                        <h2>Actions</h2>
+                        <div>
+                            <Link
+                                className="btn btn-primary mb-4"
+                                to={`/badges/${id}/edit`}
+                            >
+                                Edit
+                            </Link>
+                        </div>
+
+                        <div>
+                            <button
+                                // onClick={this.openModal}
+                                className="btn btn-danger"
+                            >
+                                Delete
+                                </button>
+                            {ReactDOM.createPortal(<h1>Hola estoy aqui, pero no estoy aqui</h1>, document.getElementById('modal'))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default BadgeDetails;
