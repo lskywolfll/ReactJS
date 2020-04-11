6cb457bc59654685e1d17d7145fa3940cc5dae77
lskywolfll <renealejandrosanchezmorales@gmail.com>
2020-04-10 23:12:52 -0400

    se deja maqueta para el nuevo backend a utilizar de consumo

--- src/api.js
+++ src/api.js
@@ -1,14 +1,6 @@
 const BASE_URL = 'http://localhost:3001';
 
-const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
-const randomNumber = (min = 0, max = 1) =>
-  Math.floor(Math.random() * (max - min + 1)) + min;
-const simulateNetworkLatency = (min = 30, max = 1500) =>
-  delay(randomNumber(min, max));
-
 async function callApi(endpoint, options = {}) {
-  await simulateNetworkLatency();
-
   options.headers = {
     'Content-Type': 'application/json',
     Accept: 'application/json',
@@ -25,27 +17,27 @@ const api = {
   badges: {
     list() {
       // throw new Error('500: Server Error');
-      return callApi('/badges');
+      return callApi('/asistant');
     },
     create(badge) {
       // throw Error('Server error');
-      return callApi(`/badges`, {
+      return callApi(`/asistant`, {
         method: 'POST',
         body: JSON.stringify(badge),
       });
     },
     read(badgeId) {
-      return callApi(`/badges/${badgeId}`);
+      return callApi(`/asistant/${badgeId}`);
     },
     update(badgeId, updates) {
-      return callApi(`/badges/${badgeId}`, {
+      return callApi(`/asistant/${badgeId}`, {
         method: 'PUT',
         body: JSON.stringify(updates),
       });
     },
     // Lo hubiera llamado `delete`, pero `delete` es un keyword en JavaScript asi que no es buena idea :P
     remove(badgeId) {
-      return callApi(`/badges/${badgeId}`, {
+      return callApi(`/asistant/${badgeId}`, {
         method: 'DELETE',
       });
     },
